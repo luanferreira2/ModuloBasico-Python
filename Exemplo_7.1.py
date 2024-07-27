2d2d64f753c20e99072128ecd91cab2da663d2a0
luanferreira2 <luan.silva115@fatec.sp.gov.br>
2024-07-27 16:06:53 -0300

    lista

---
+++ Exemplo_7.1.py
@@ -0,0 +1,17 @@
+lista = [36, 25, 21, 48, 17, 9, 16, 23, 29, 31]
+
+print(" Exibição completa da lista")
+
+print(lista)
+
+print(' Exibição dos elementos individuais')
+
+i = 0
+
+while i < len(lista):
+
+    print(lista[i], end='  ')
+
+    i = i + 1
+
+print('\n Fim do Programa')
