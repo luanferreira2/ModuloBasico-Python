2e84f043e0fa312e7618dfc7392ce90d43872a90
luanferreira2 <luan.silva115@fatec.sp.gov.br>
2024-07-29 13:36:59 -0300

    for

---
+++ Exemplo_7.2.py
@@ -0,0 +1,8 @@
+lista = [12, 32, 64, 89]
+posicao = 0
+
+for valor in lista:
+    print(f' A posição numero {posicao} contém o valor {valor}')
+    posicao = posicao + 1
+
+print(' Fim do Programa')
